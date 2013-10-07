fc3481f7c861e8fac924ff9fcd2a7f175d514348
jeando <favreau@polytech.unice.fr>
2013-10-07 14:55:13 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 template<typename T>
 void normalized_gray_image(cv::Mat_<T>& mat, const double new_max)
 {
- //   using T = float;
+    //   using T = float;
     T max_val(*mat[0]);
     T min_val = max_val;
     const int n_row = mat.rows;
@@ -51,22 +51,16 @@ void normalized_gray_image(cv::Mat_<T>& mat, const double new_max)
         }
     }
 }
-int main()
+
+template<typename T>
+void ondelette_1(const cv::Mat_<T>& br, std::vector<cv::Mat_<int>>& img)
 {
-    cv::Mat input = cv::imread("img/lena.bmp",CV_LOAD_IMAGE_COLOR);
-    if(input.rows==0 && input.cols==0)
-    {
-        std::cerr << "unable to load the image" << std::endl;
-        return EXIT_FAILURE;
-    }
-    cv::Mat_<uchar> br;
-    std::vector<cv::Mat_<int>> img;
-    cv::cvtColor(input,br,CV_BGR2GRAY);
     const int cols = br.cols;
     const int rows = br.rows;
     const int cols_s_2 = cols>>1;
     const int rows_s_2 = rows>>1;
 
+    img.clear();
     img.push_back(cv::Mat_<int>(rows_s_2,cols_s_2));
     img.push_back(cv::Mat_<int>(rows_s_2,cols_s_2));
     img.push_back(cv::Mat_<int>(rows_s_2,cols_s_2));
@@ -75,10 +69,10 @@ int main()
     for(int row=0; row<rows_s_2; row++)
     {
         const int row_x2 = row << 1;
-        uchar* ptr11 = br[row_x2];
-        uchar* ptr12 = ptr11+1;
-        uchar* ptr21 = br[row_x2+1];
-        uchar* ptr22 = ptr21+1;
+        const T* ptr11 = br[row_x2];
+        const T* ptr12 = ptr11+1;
+        const T* ptr21 = br[row_x2+1];
+        const T* ptr22 = ptr21+1;
 
         int* ptr_img0 = img[0][row];
         int* ptr_img1 = img[1][row];
@@ -100,23 +94,26 @@ int main()
             *ptr_img3++ = val11 - val12 - val21 + val22;
         }
     }
-
-
-
-
-    cv::Mat_<uchar> br2 = cv::Mat_<uchar>(rows,cols);
+}
+template<typename T>
+void ondelette_1_synthese(const std::vector<cv::Mat_<int>>& img, cv::Mat_<T>& br2)
+{
+    const int cols = br2.cols;
+    const int rows = br2.rows;
+    const int cols_s_2 = cols>>1;
+    const int rows_s_2 = rows>>1;
     for(int row=0; row<rows_s_2; row++)
     {
         const int row_x2 = row << 1;
-        uchar* ptr11 = br2[row_x2];
-        uchar* ptr12 = ptr11+1;
-        uchar* ptr21 = br2[row_x2+1];
-        uchar* ptr22 = ptr21+1;
+        T* ptr11 = br2[row_x2];
+        T* ptr12 = ptr11+1;
+        T* ptr21 = br2[row_x2+1];
+        T* ptr22 = ptr21+1;
 
-        int* ptr_img0 = img[0][row];
-        int* ptr_img1 = img[1][row];
-        int* ptr_img2 = img[2][row];
-        int* ptr_img3 = img[3][row];
+        const int* ptr_img0 = img[0][row];
+        const int* ptr_img1 = img[1][row];
+        const int* ptr_img2 = img[2][row];
+        const int* ptr_img3 = img[3][row];
         for(int col=0; col<cols_s_2; col++)
         {
             const int val11 = *ptr_img0++;
@@ -133,6 +130,69 @@ int main()
             ptr22+=2;
         }
     }
+}
+template<typename S,typename T>
+void coller_image(cv::Mat_<S>& dst, const cv::Mat_<T>& src1,const cv::Mat_<T>& src2,const cv::Mat_<T>& src3,const cv::Mat_<T>& src4)
+{
+    const int cols = src1.cols;
+    const int rows = src1.rows;
+    dst = cv::Mat_<S>(rows<<1,cols<<1);
+
+    S* ptr_img_1 = dst[0];
+    S* ptr_img_2 = dst[rows];
+    for(int row=0; row<rows; row++)
+    {
+        const T* ptr_img0 = src1[row];
+        const T* ptr_img2 = src3[row];
+        for(int col=0; col<cols; col++)
+        {
+            *ptr_img_1++=*ptr_img0++;
+            *ptr_img_2++=*ptr_img2++;
+        }
+
+        const T* ptr_img1 = src2[row];
+        const T* ptr_img3 = src4[row];
+        for(int col=0; col<cols; col++)
+        {
+            *ptr_img_1++=*ptr_img1++;
+            *ptr_img_2++=*ptr_img3++;
+        }
+    }
+}
+
+int main()
+{
+    cv::Mat input = cv::imread("img/lena.bmp",CV_LOAD_IMAGE_COLOR);
+    if(input.rows==0 && input.cols==0)
+    {
+        std::cerr << "unable to load the image" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const int cols = input.cols;
+    const int rows = input.rows;
+    const int cols_s_2 = cols>>1;
+    const int rows_s_2 = rows>>1;
+
+    cv::Mat_<uchar> br;
+    std::vector<std::vector<cv::Mat_<int>>> img(6);
+
+    cv::cvtColor(input,br,CV_BGR2GRAY);
+
+    ondelette_1(br,img[0]);
+    for(int i=1; i<img.size(); i++)
+    {
+        ondelette_1(img[i-1][0],img[i]);
+    }
+
+
+    cv::Mat_<uchar> br2 = cv::Mat_<uchar>(rows,cols);
+
+    for(int i=img.size()-1; i>0; i--)
+    {
+        ondelette_1_synthese(img[i],img[i-1][0]);
+    }
+    ondelette_1_synthese(img[0],br2);
     cv::namedWindow("input", CV_WINDOW_NORMAL );
     cv::namedWindow("dst1", CV_WINDOW_NORMAL );
     cv::namedWindow("dst2", CV_WINDOW_NORMAL );
@@ -141,19 +201,48 @@ int main()
     cv::imshow("input",br);
     for(int i=0; i<4; i++)
     {
-        normalized_gray_image(img[i],65535);//because If the image is 16-bit unsigned or 32-bit integer, the pixels are divided by 256
+        normalized_gray_image(img[0][i],65535);//because If the image is 16-bit unsigned or 32-bit integer, the pixels are divided by 256
     }
-    cv::imshow("dst1",img[0]);
-    cv::imshow("dst2",img[1]);
-    cv::imshow("dst3",img[2]);
-    cv::imshow("dst4",img[3]);
-    cv::imwrite("img/img11.bmp",img[0]/255);
-    cv::imwrite("img/img12.bmp",img[1]/255);
-    cv::imwrite("img/img21.bmp",img[2]/255);
-    cv::imwrite("img/img22.bmp",img[3]/255);
+    cv::imshow("dst1",img[0][0]);
+    cv::imshow("dst2",img[0][1]);
+    cv::imshow("dst3",img[0][2]);
+    cv::imshow("dst4",img[0][3]);
+    img[0][0]/=255;
+    img[0][1]/=255;
+    img[0][2]/=255;
+    img[0][3]/=255;
+    cv::imwrite("img/img11.bmp",img[0][0]);
+    cv::imwrite("img/img12.bmp",img[0][1]);
+    cv::imwrite("img/img21.bmp",img[0][2]);
+    cv::imwrite("img/img22.bmp",img[0][3]);
     cv::imwrite("img/image_reconstruite.bmp",br2);
     cv::namedWindow("br2", CV_WINDOW_NORMAL );
     cv::imshow("br2",br2);
+
+    cv::Mat_<uchar> img_ensemble(rows,cols);
+
+
+
+    for(int i=0; i<img.size(); i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            normalized_gray_image(img[i][j],255);
+        }
+    }
+    for(int i=0; i<img.size(); i++)
+    {
+        for(int j=i; j>0; j--)
+        {
+            coller_image(img[j-1][0],img[j][0],img[j][1],img[j][2],img[j][3]);
+        }
+        coller_image(img_ensemble,img[0][0],img[0][1],img[0][2],img[0][3]);
+        std::ostringstream oss;
+        oss << "img/rec" << (i+1) << ".bmp";
+        cv::imwrite(oss.str().c_str(),img_ensemble);
+    }
+    cv::namedWindow("img_ensemble", CV_WINDOW_NORMAL );
+    cv::imshow("img_ensemble",img_ensemble);
     cv::namedWindow("diff", CV_WINDOW_NORMAL );
     cv::Mat_<uchar> diff = br2-br;
     normalized_gray_image(diff,255);
